9abee6750e27d2d790e121aa3f8ad0de4fdf932a
gianella12 <gianellalastra4@gmail.com>
2026-05-22 20:15:46 -0300

    feat(worker, db) add onConflictDoNothing to flush and dedupe integration test

--- apps/worker/src/processors/metrics-ingest.ts
+++ apps/worker/src/processors/metrics-ingest.ts
@@ -18,7 +18,8 @@ export async function flush() {
         value: item.metricValue,
         hostname: item.hostName,
       })),
-    );
+    )
+    .onConflictDoNothing()
 }
 setInterval(async () => {
   await flush();
