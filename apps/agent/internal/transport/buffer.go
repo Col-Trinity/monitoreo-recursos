deeb8e7dba07eedbed6c2b6125661c9858bb661d
gianella12 <gianellalastra4@gmail.com>
2026-06-03 13:36:43 -0300

    fix(agent/transport): add comment to BufferMetrics struct

--- apps/agent/internal/transport/buffer.go
+++ apps/agent/internal/transport/buffer.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Col-Trinity/monitoreo-recursos/apps/agent/internal/protocol"
 )
 
+// BufferMetrics stores metrics containers indexed by timestamp with a max capacity
 type BufferMetrics struct {
 	mu          sync.Mutex
 	metricsDict map[int64]protocol.MetricsContainer
